8197108d50df91a8642a7a2db0e71ff61326f49c
Neil Simmons <45835168+Burgerland@users.noreply.github.com>
2019-12-08 23:14:45 -0500

    Add files via upload

---
+++ Stock Explorer/setup.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import csv
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+#connect to postgres
+try:
+	conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='password'")
+except:
+	print "can't connect to database"
+	exit()
+
+
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = conn.cursor() 
+
+
+#create database, drop old version if it exists
+try:
+	cur.execute("""DROP DATABASE SandP500""")
+except:
+	print cur.statusmessage
+try:
+	cur.execute("""CREATE DATABASE SandP500""")
+except:
+	print cur.statusmessage
+#connect to new database
+conn = psycopg2.connect("dbname='sandp500' user='postgres' host='localhost' password='password'")
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cur = conn.cursor()
+
+#define list of stocks
+stocks = ['MMM','ABT','ABBV','ACN','ATVI','AYI','ADBE','AMD','AAP','AES','AET',
+		'AMG','AFL','A','APD','AKAM','ALK','ALB','ARE','ALXN','ALGN','ALLE',
+		'AGN','ADS','LNT','ALL','GOOGL','GOOG','MO','AMZN','AEE','AAL','AEP',
+		'AXP','AIG','AMT','AWK','AMP','ABC','AME','AMGN','APH','APC','ADI','ANDV',
+		'ANSS','ANTM','AON','AOS','APA','AIV','AAPL','AMAT','APTV','ADM','ARNC',
+		'AJG','AIZ','T','ADSK','ADP','AZO','AVB','AVY','BHGE','BLL','BAC','BK',
+		'BAX','BBT','BDX','BRK.B','BBY','BIIB','BLK','HRB','BA','BWA','BXP','BSX',
+		'BHF','BMY','AVGO','BF.B','CHRW','CA','COG','CDNS','CPB','COF','CAH','CBOE',
+		'KMX','CCL','CAT','CBG','CBS','CELG','CNC','CNP','CTL','CERN','CF','SCHW',
+		'CHTR','CHK','CVX','CMG','CB','CHD','CI','XEC','CINF','CTAS','CSCO','C','CFG',
+		'CTXS','CLX','CME','CMS','KO','CTSH','CL','CMCSA','CMA','CAG','CXO','COP',
+		'ED','STZ','COO','GLW','COST','COTY','CCI','CSRA','CSX','CMI','CVS','DHI',
+		'DHR','DRI','DVA','DE','DAL','XRAY','DVN','DLR','DFS','DISCA','DISCK','DISH',
+		'DG','DLTR','D','DOV','DWDP','DPS','DTE','DRE','DUK','DXC','ETFC','EMN','ETN',
+		'EBAY','ECL','EIX','EW','EA','EMR','ETR','EVHC','EOG','EQT','EFX','EQIX','EQR',
+		'ESS','EL','ES','RE','EXC','EXPE','EXPD','ESRX','EXR','XOM','FFIV','FB','FAST',
+		'FRT','FDX','FIS','FITB','FE','FISV','FLIR','FLS','FLR','FMC','FL','F','FTV',
+		'FBHS','BEN','FCX','GPS','GRMN','IT','GD','GE','GGP','GIS','GM','GPC','GILD',
+		'GPN','GS','GT','GWW','HAL','HBI','HOG','HRS','HIG','HAS','HCA','HCP','HP','HSIC',
+		'HSY','HES','HPE','HLT','HOLX','HD','HON','HRL','HST','HPQ','HUM','HBAN','HII',
+		'IDXX','INFO','ITW','ILMN','IR','INTC','ICE','IBM','INCY','IP','IPG','IFF','INTU',
+		'ISRG','IVZ','IQV','IRM','JEC','JBHT','SJM','JNJ','JCI','JPM','JNPR','KSU','K','KEY',
+		'KMB','KIM','KMI','KLAC','KSS','KHC','KR','LB','LLL','LH','LRCX','LEG','LEN','LUK',
+		'LLY','LNC','LKQ','LMT','L','LOW','LYB','MTB','MAC','M','MRO','MPC','MAR','MMC','MLM',
+		'MAS','MA','MAT','MKC','MCD','MCK','MDT','MRK','MET','MTD','MGM','KORS','MCHP','MU',
+		'MSFT','MAA','MHK','TAP','MDLZ','MON','MNST','MCO','MS','MOS','MSI','MYL','NDAQ',
+		'NOV','NAVI','NTAP','NFLX','NWL','NFX','NEM','NWSA','NWS','NEE','NLSN','NKE','NI',
+		'NBL','JWN','NSC','NTRS','NOC','NCLH','NRG','NUE','NVDA','ORLY','OXY','OMC','OKE',
+		'ORCL','PCAR','PKG','PH','PDCO','PAYX','PYPL','PNR','PBCT','PEP','PKI','PRGO','PFE',
+		'PCG','PM','PSX','PNW','PXD','PNC','RL','PPG','PPL','PX','PCLN','PFG','PG','PGR',
+		'PLD','PRU','PEG','PSA','PHM','PVH','QRVO','PWR','QCOM','DGX','RRC','RJF','RTN','O',
+		'RHT','REG','REGN','RF','RSG','RMD','RHI','ROK','COL','ROP','ROST','RCL','CRM','SBAC',
+		'SCG','SLB','SNI','STX','SEE','SRE','SHW','SIG','SPG','SWKS','SLG','SNA','SO','LUV',
+		'SPGI','SWK','SBUX','STT','SRCL','SYK','STI','SYMC','SYF','SNPS','SYY','TROW','TPR',
+		'TGT','TEL','FTI','TXN','TXT','TMO','TIF','TWX','TJX','TMK','TSS','TSCO','TDG','TRV',
+		'TRIP','FOXA','FOX','TSN','UDR','ULTA','USB','UAA','UA','UNP','UAL','UNH','UPS','URI',
+		'UTX','UHS','UNM','VFC','VLO','VAR','VTR','VRSN','VRSK','VZ','VRTX','VIAB','V','VNO',
+		'VMC','WMT','WBA','DIS','WM','WAT','WEC','WFC','HCN','WDC','WU','WRK','WY','WHR','WMB',
+		'WLTW','WYN','WYNN','XEL','XRX','XLNX','XL','XYL','YUM','ZBH','ZION','ZTS']
+
+
+create_good_table = "CREATE TABLE good_stocks (name TEXT PRIMARY KEY NOT NULL)"
+cur.execute(create_good_table)
+#iterate through stocks
+for stock in stocks:
+
+	print "\nCreating " + stock + " table"
+	#define the table creation string
+	create_table = ("CREATE TABLE " + stock +
+		" (date DATE PRIMARY KEY NOT NULL, " +
+		"open NUMERIC(10,2) NOT NULL," +
+		"high NUMERIC(10,2) NOT NULL," +
+		"low NUMERIC(10,2) NOT NULL,"  +
+		"close NUMERIC(10,2) NOT NULL,"+
+		"volume INTEGER NOT NULL," +
+		"name TEXT NOT NULL)")
+	#try to do all operations required to populate a table
+	try:
+		data = open("./individual_stocks_5yr/" + stock + "_data.csv", 'r')
+		reader = csv.reader(data)
+		print reader.next()
+		cur.execute(create_table)
+		table_made = 1;
+	#catch failure
+	except:
+		print cur.statusmessage
+		print ("Table " + stock + " not created")
+		table_made = 0;
+
+	#if the table was made
+	if table_made == 1:
+		good_string =("INSERT INTO good_stocks (name) VALUES('" + stock + "')")
+		try:
+			
+		
+			#define the insertion string
+			copy = ("INSERT INTO " + stock + 
+				" VALUES (to_timestamp(%s, 'yyyy-mm-dd'), " +
+				"%s, %s, %s, %s, %s, %s)")
+			
+			#insert every source row into the destination
+			for row in reader:
+				cur.execute(copy, row)
+				if cur.statusmessage != "INSERT 0 1":
+					print cur.statusmessage
+			cur.execute(good_string)
+		except:
+			cur.execute("DROP TABLE " + stock)
+			print "Table " + stock + " aborted"
+	
+
+
